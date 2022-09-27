de58d8ca6b677e88e660f2ad02a6c91d234bd59d
JosefVodicka <josef.vodicka@yahoo.com>
2022-09-26 22:19:26 -0400

    Twitter

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,79 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Josef Vodicka on 9/25/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweet()
+    }
+    
+    func loadTweet(){
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        
+
+        let myParams = ["count": 10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+           
+            print("Couldnt retreive tweets!")
+            print(Error.localizedDescription)
+        })
+        
+    }
+    
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell",for: indexPath) as! TweetCellTableViewCell
+        
+        cell.userNameLabel.text = "Some name"
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        return cell
+    }
+    
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweetArray.count
+    }
+
+    
+
+}
